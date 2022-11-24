5996205a3c53f02bbc4b23d5a84f2763029e4a87
Gabriel Garcia <89267342+gabito1966@users.noreply.github.com>
2022-11-24 15:26:03 -0300

    fin del programa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de Usuarios';
+  mensaje='';
+  registrado = false;
+  nombre:String="";
+  apellido:String="";
+  entradas: any;
+
+
+  constructor(){
+    this.entradas=[
+
+      {titulo:"Python cada dia más presente"},
+      {titulo:"Java presente desde hace más de 20 años"},
+      {titulo:"Javascript cada vez más funcional"},
+      {titulo:"Kotlin potencia para tus app"},
+      {titulo:"¿Donde quedo Pascal?"},
+
+    ]
+  }
+
+  registrarUsuario(){
+    this.registrado=true
+    this.mensaje = 'Usuario registrado con exito';
+  }
 }
